b1163cdc61234a283a4667b02d666571983f78bd
luzclaritayllanesramos-star <luzclaritayllanesramos@gmail.com>
2026-07-16 19:42:46 -0500

    Add files via upload

---
+++ SEMANA 4 INSTALANDO PAQUETESS.R
@@ -0,0 +1,19 @@
+#Istalndo paquetes
+
+install.packages("ggplot2")
+library(ggplot2)
+
+install.packages("dplyr")
+library(dplyr)
+
+install.packages("readr")
+library(readr)
+
+install.packages("janitor")
+library(janitor)
+
+install.packages("stringr")
+library(stringr)
+
+install.packages("forcats")
+library(forcats)
